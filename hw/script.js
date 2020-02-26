763927490cbcff076fa91ee8ee0cb9755d4bbc87
Lol12399 <55404515+Lol12399@users.noreply.github.com>
2020-02-27 00:14:59 +0200

    Add files via upload

---
+++ hw/script.js
@@ -0,0 +1,109 @@
+'use strict';
+
+document.body.style.cssText = 'background: url(bg.jpg) no-repeat'
+
+let div = document.createElement('div');
+div.classList.add('div');
+
+let img0 = document.createElement('img');
+img0.style.cssText = 'position: relative;';
+
+let img1 = document.createElement('img');
+img1.classList.add('i1')
+img1.setAttribute('src', 'clipart1.png' )
+img1.setAttribute('name', 'name' )
+img1.style.cssText = 'width: 15%; position: absolute; top: 5px; left: 700px;';
+
+let img2 = document.createElement('img');
+img2.classList.add('i2')
+img2.setAttribute('src', '1.png')
+img2.style.cssText = 'width: 150px; position: absolute; top: 40px; left: 950px;';
+
+let img3 = document.createElement('img');
+img3.classList.add('i3')
+img3.setAttribute('src', 'clipart2.png')
+img3.style.cssText = 'width: 190px; position: absolute; top: 100px; left: 1100px;';
+
+let img4 = document.createElement('img');
+img4.classList.add('i4')
+img4.setAttribute('src', '2.png')
+img4.style.cssText = 'width: 17%; position: absolute; top: 150px; left: 1200px;';
+
+let img5 = document.createElement('img');
+img5.classList.add('i5')
+img5.setAttribute('src', 'clipart3.png')
+img5.style.cssText = 'width: 30px; position: absolute; left: 480px; top: 30px;';
+
+let img6 = document.createElement('img');
+img6.classList.add('i6')
+img6.setAttribute('src', '3.png')
+img6.style.cssText = 'width: 325px; bottom: 135px; right: 180px; position: absolute;';
+
+let img7 = document.createElement('img');
+img7.classList.add('i7')
+img7.setAttribute('src', 'clipart4.png')
+img7.style.cssText = 'width: 175px; bottom: 45px; left: 950px; position: absolute;';
+
+let img8 = document.createElement('img');
+img8.classList.add('i8')
+img8.setAttribute('src', '4.png')
+img8.style.cssText = 'width: 200px; bottom: 20px; left: 700px; position: absolute;';
+
+let img9 = document.createElement('img');
+img9.classList.add('i9')
+img9.setAttribute('src', 'clipart5.png')
+img9.style.cssText = 'width: 200px; bottom: 10px; left: 470px; position: absolute;';
+
+let img10 = document.createElement('img');
+img10.classList.add('i10')
+img10.setAttribute('src', '5.png')
+img10.style.cssText = 'width: 200px; bottom: 100px; left: 250px; position: absolute;';
+
+let img11 = document.createElement('img');
+img11.classList.add('i11')
+img11.setAttribute('src', 'clipart6.png')
+img11.style.cssText = 'width: 200px; top: 200px; left: 270px; position: absolute;';
+
+let img12 = document.createElement('img');
+img12.classList.add('i12')
+img12.setAttribute('src', '6.png')
+img12.style.cssText = 'width: 175px; top: 30px; left: 480px; position: absolute;';
+
+let img13 = document.createElement('img');
+img13.classList.add('i13')
+img13.setAttribute('src', '7.png')
+img13.style.cssText = 'width: 300px; top: 220px; left: 700px; position: absolute;';
+
+let img14 = document.createElement('img');
+img14.classList.add('i14')
+img14.setAttribute('src', 'clipart7.png')
+img14.style.cssText = 'width: 205px; position: absolute; bottom: 0px; left: 1px;';
+
+let img15 = document.createElement('img');
+img15.classList.add('i15')
+img15.setAttribute('src', '8.png')
+img15.style.cssText = 'width: 170px; position: absolute; bottom: 0px; left: 0px;';
+
+
+
+document.body.append(div);
+div.append(img0);
+div.append(img1);
+div.append(img2);
+div.append(img3);
+div.append(img4);
+div.append(img5);
+div.append(img6);
+div.append(img7);
+div.append(img8);
+div.append(img9);
+div.append(img10);
+div.append(img11);
+div.append(img12);
+div.append(img13);
+div.append(img14);
+div.append(img15);
+
+
+// document.createElement('div');
+// document.body.dispat
